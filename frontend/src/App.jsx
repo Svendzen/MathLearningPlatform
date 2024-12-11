00719c2f4c2e5954f4b2a83c7570b036c4460c3a
Johan Svendsen <johan.svendsen@hotmail.com>
2024-12-11 16:42:33 +0100

    Added BrowserRouter

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,10 @@
 
 function App() {
-
-  return (
-    <>
-      <h1>Hello World!</h1>
-    </>
-  )
+    return (
+        <>
+            <h1>Hello World!</h1>
+        </>
+    )
 }
 
 export default App
